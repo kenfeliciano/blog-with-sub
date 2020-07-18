bbef5380655f64dc9fc8656c4328d8b3ea8319d4
Ken Feliciano <kafeliciano@gmail.com>
2020-07-17 22:05:45 -0700

    Point content at blog-content submodule

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,14 +15,14 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/blog`,
+        path: `${__dirname}/blog-content/content/blog`,
         name: `blog`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/assets`,
+        path: `${__dirname}/blog-content/content/assets`,
         name: `assets`,
       },
     },
